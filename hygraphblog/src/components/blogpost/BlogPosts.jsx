f6450e1fcb347eda660f4cbe91fad6d327cd16d5
katrine-mo-jensen <katrine.mo.jensen@gmail.com>
2023-10-05 14:16:41 +0200

    blog image

--- hygraphblog/src/components/blogpost/BlogPosts.jsx
+++ hygraphblog/src/components/blogpost/BlogPosts.jsx
@@ -4,18 +4,16 @@ import { OneBlog } from "../../query/oneBlog";
 import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
 
-
-
 export const OneBlogPosts = () => {
-  const {id} = useParams()
+  const { id } = useParams();
 
   const { data, isLoading, error } = useQuery({
     queryKey: ["giveOneBlog"],
     queryFn: async () =>
-      request(import.meta.env.VITE_PUBLIC_URL_ID, OneBlog, {id:id}),
+      request(import.meta.env.VITE_PUBLIC_URL_ID, OneBlog, { id: id }),
   });
 
-  console.log(data)
+  console.log(data);
 
   if (isLoading) {
     return <p>Loading... </p>;
@@ -25,17 +23,15 @@ export const OneBlogPosts = () => {
     return <p>Error: {error.message}</p>;
   }
 
-
   return (
     <>
-     
       <article>
-        {/* <h3>{data.blogPosts.blogTitle}</h3>  */}
-        
+        <h3>{data.blogPost.blogTitle}</h3>
+        <p>{data.blogPost.blogDescription}</p>
+        {/* <img src={data.blogPost.blogImage[0].url} alt={data.blogPost.blogTitle} /> */}
       </article>
       <button>
-        {" "}
-        <Link to="/blogposts">BACK</Link>{" "}
+        <Link to="/blogposts">BACK</Link>
       </button>
     </>
   );
